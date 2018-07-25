b188d268e61e8f867a74a833ceb6ffffeed69a2e
Thomas Sharkey <tbsharkey@gmail.com>
2018-07-25 02:54:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(){
+  
+}
+
+function distanceFromHqInFeet(){
+  
+}
+
+function distanceFromHqInBlocks(){
+  
+}
+
+function calculatesFarePrice(start, destination){
+  
+}
